1d8fcb95d2caafdb081a7b510112ea473ef3b559
adriannaRodencal <adrianna.rodencal@llwarriors.org>
2020-04-05 13:03:50 -0500

    class Deck from CardCollection + unittest

--- deck.py
+++ deck.py
@@ -1,6 +1,28 @@
+from card import Card
+from cardCollection import CardCollection
 
-class Deck(object):
+class Deck(CardCollection):
 
     def __init__(self):
+        super().__init__()
+        for name in Card.names:
+            for suit in Card.suits:
+                newCard = Card(name, suit)
+                self.append(newCard)
 
-    def __str__(self):
+    def stack(self, deckFile = 'deck.txt'):
+        """
+        Clear the current deck and restack a new deck.
+        :param deckFile
+        :return none
+        """
+        #
+        # Clear the current list to restack
+        #
+        self.clear()
+        with open(deckFile, 'r') as endFile:
+            for line in endFile:
+                suit, name = line.split(', ')
+                card = Card(name.strip(), suit)
+                self.append(card)
+
